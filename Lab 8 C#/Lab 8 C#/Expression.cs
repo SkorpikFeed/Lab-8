cf0afd3b8a04b0dd9a1a5c4b5a0423e3385a2dee
SkorpikFeed <125889197+SkorpikFeed@users.noreply.github.com>
2023-05-23 22:49:32 +0300

    Add files via upload

---
+++ Lab 8 C#/Lab 8 C#/Expression.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Lab_8_C_
+{
+    internal class Expression
+    {
+        private double a;
+        private double b; 
+        private double c; 
+        private double d;
+        public Expression(double a, double b, double c, double d)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+            this.d = d;
+        }
+        public double A { get { return a; } set { a = value; } }
+        public double B { get { return b; } set { b = value; } }
+        public double C { get { return c; } set { c = value; } }
+        public double D { get { return d; } set { d = value; } }
+        public string Calculate()
+        {
+            try
+            {
+                double res = (Math.Log((4 * b) - c) * a) / (b + (c / d) - 1);
+                if (d == 0) 
+                {
+                    throw new Exception("Operand d cannot be 0");
+                }
+                if ((4 * b) - c == 0) 
+                {
+                    throw new Exception("The logarithm argument cannot be 0");
+                }
+                if ((4 * b) - c < 0) 
+                {
+                    throw new Exception("The logarithm argument cannot be negative");
+                }
+                if (b + (c / d) - 1 == 0) 
+                {
+                    throw new Exception("You cannot divide by 0");
+                }
+                return res.ToString();
+            }
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(e.Message);
+                Console.ResetColor();
+                return "Your expression doesn`t make any sence";
+            }
+        }
+        public void Print()
+        {
+            Console.WriteLine("Result: " + Calculate());
+        }
+    }
+}
---
+++ Lab 8 C#/Lab 8 C#/Program.cs
@@ -0,0 +1,45 @@
+﻿using Lab_8_C_;
+
+class Program
+{
+    static void Main()
+    {
+        List<Expression> expressions = new List<Expression>();
+    start:
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Enter the operands:");
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.Write("A:");
+        double a = double.Parse(Console.ReadLine());
+        Console.Write("B:");
+        double b = double.Parse(Console.ReadLine());
+        Console.Write("C:");
+        double c = double.Parse(Console.ReadLine());
+        Console.Write("D:");
+        double d = double.Parse(Console.ReadLine());
+        Expression expr = new Expression(a, b, c, d);
+        Console.WriteLine("Your operands: a({0}), b({1}), c({2}), d({3})", a, b, c, d);
+        expressions.Add(expr);
+        while (true)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Enter 1 if you want to get the result");
+            Console.WriteLine("Enter 2 if you want to change operands");
+            Console.ForegroundColor = ConsoleColor.White;
+            string choice = Console.ReadLine();
+            if (choice == "1")
+            {
+                expr.Print();
+            }
+            else if (choice == "2")
+            {
+                Console.Clear();
+                goto start;
+            }
+            else
+            {
+                Console.WriteLine("Wrong input");
+            }
+        }
+    }
+}
